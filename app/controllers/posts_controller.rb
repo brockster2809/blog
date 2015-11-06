6fd533a94bd7ba50183fada33bf699428d4d0bd5
anshul goyal <anshulgoyal2809@gmail.com>
2015-11-06 16:25:49 +0530

    edit and delete actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,28 @@ def create
 	end
 end
 
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	@post = Post.find(params[:id])
+
+	if @post.update(params[:post].permit(:title,:body))
+		redirect_to @post
+	else
+		render 'edit'
+	end
+end
+
+def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+
+	redirect_to posts_path
+end
+
+
 def post_params
 	params.require(:post).permit(:title, :body)
 end
